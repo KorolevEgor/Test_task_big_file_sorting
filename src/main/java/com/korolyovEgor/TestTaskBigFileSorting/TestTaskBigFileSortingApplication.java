f2c39bda1f8579ed7af8eb33015bccb7acc39072
KorolevEgor <korolevegir@gmail.com>
2022-07-20 15:42:02 +0300

    init

---
+++ src/main/java/com/korolyovEgor/TestTaskBigFileSorting/TestTaskBigFileSortingApplication.java
@@ -0,0 +1,13 @@
+package com.korolyovEgor.TestTaskBigFileSorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskBigFileSortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskBigFileSortingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korolyovEgor/TestTaskBigFileSorting/TestTaskBigFileSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korolyovEgor.TestTaskBigFileSorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskBigFileSortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
